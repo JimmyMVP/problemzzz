17db24cb220668e7ff738fa13e9256642603114b
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2015-12-02 16:25:52 +0100

    Added solution for negative numbers in integer products, need to add solution for k numbers

--- integer_lists.py
+++ integer_lists.py
@@ -2,7 +2,7 @@ import random
 from collections import deque
 import cProfile
 
-test = [random.randint(1,10000) for i in range(0,100000)]
+test = [random.randint(1,1000) for i in range(0,100000)]
 
 
 #Find product of all numbers in array for index, except the index
@@ -22,25 +22,44 @@ def integer_products(l):
 	return init1
 
 
-#Find highest product of three numbers in array
-#Implement for negative numbers also?
+#Find highest product of three numbers in array O(n) time complexity,
+#with support for negative numbers
+#Do for k items? O(1) space complexity?
 def highest_product_of_three(l):
 
 	fifo = deque()
 	
-	fifo.append(0)
-	fifo.append(0)
-	fifo.append(0)
+	fifo.append(1)
+	fifo.append(1)
+	fifo.append(1)
+
+	fifo2 = deque()
+	fifo2.append(-1)
+	fifo2.append(-1)
+
+
 
 	for i in l:
 		if fifo[-1] < i:
+			fifo.pop()
 			fifo.appendleft(i)
+		if fifo2[-1] > i:
+			fifo2.pop()
+			fifo2.appendleft(i)
+
+
 
 	max_product = 1
+	max_product_neg = 1
 	for i in fifo:
-
 		max_product *= i
 
-	return max_product
+	for i in fifo2:
+		max_product *= i
+
+	max_product_neg *= fifo[0]
+
+	return max(max_product, max_product_neg)
+
 
 cProfile.run("highest_product_of_three(test)")
