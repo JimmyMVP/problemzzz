af28f9672a38678422dc42652826f0289114319b
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2015-12-02 19:21:54 +0100

    Fixed error from previous implementation with FIFO

---
+++ condense_meetings.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+def condense_meetings(meetings):
+
+	condensed = []
+	for meeting in meetings:
+
+		
--- integer_lists.py
+++ integer_lists.py
@@ -2,7 +2,7 @@ import random
 from collections import deque
 import cProfile
 
-test = [random.randint(1,1000) for i in range(0,100000)]
+test = [4,1,4,2,5,3,2,1]
 
 
 #Find product of all numbers in array for index, except the index
@@ -25,41 +25,33 @@ def integer_products(l):
 #Find highest product of three numbers in array O(n) time complexity,
 #with support for negative numbers
 #Do for k items? O(1) space complexity?
-def highest_product_of_three(l):
+#No FIFO, the second biggest element can end up on the end
+def highest_product_of_three(l, k):
 
-	fifo = deque()
-	
-	fifo.append(1)
-	fifo.append(1)
-	fifo.append(1)
+	l1 = sorted([l[0], l[1], l[2]])
+	l2 = []
 
-	fifo2 = deque()
-	fifo2.append(-1)
-	fifo2.append(-1)
-
-
-
-	for i in l:
-		if fifo[-1] < i:
-			fifo.pop()
-			fifo.appendleft(i)
-		if fifo2[-1] > i:
-			fifo2.pop()
-			fifo2.appendleft(i)
+	for i in range(0, len(l)):
+		for j in range(0,k):
+			if(l[i] > l1[j]):
+				l1[j] = l[i]
+				break
 
 
 
 	max_product = 1
 	max_product_neg = 1
-	for i in fifo:
+	for i in l1:
 		max_product *= i
 
-	for i in fifo2:
-		max_product *= i
+#	for i in fifo2:
+#		max_product *= i
 
-	max_product_neg *= fifo[0]
+#	max_product_neg *= fifo[0]
 
-	return max(max_product, max_product_neg)
+	return max_product
 
 
-cProfile.run("highest_product_of_three(test)")
+print highest_product_of_three(test, 3)
+
+#cProfile.run("highest_product_of_three(test)")
