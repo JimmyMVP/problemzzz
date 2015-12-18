5718587153bf19c57858f6666ab9652c9237bdd8
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2015-12-18 16:49:05 +0100

    Added a faster but memory ineffective solution of two sum

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,5 +1,15 @@
 #include <map>
 
+
+// Given an array of integers, find two numbers such that they add up to a specific target number.
+
+// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+// You may assume that each input would have exactly one solution.
+
+// Input: numbers={2, 7, 11, 15}, target=9
+// Output: index1=1, index2=2
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -39,6 +49,49 @@ public:
                 
         }
        
+        
+    }
+};
+
+
+//Is this solution memory ineffective? Faster than the previous
+class Solution2 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> solution;
+        int min, max;
+        
+        min = target;
+        max = nums[0];
+        for(int num : nums) {
+            if(num < min) {
+                min = num;
+            }
+            if(num > max) {
+                max = num;
+            }
+        }
+        int a[max-min];
+        
+        for(int i = 0; i < max-min; i++) {
+            a[i] = -1;
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            a[nums[i]-min] = i;
+        }
+        int bet = target - min;
+        int d = max-min;
+        for(int i = 0; i < nums.size();i++) {
+            int p = bet - nums[i];
+            if(p > d || p < 0) continue;
+            int v1 = a[p];
+            if(v1 != i && v1 >= 0) {
+                solution.push_back(i+1);
+                solution.push_back(v1+1);
+                return solution;
+            }
+        }
         
     }
 };
